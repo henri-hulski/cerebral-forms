454e3eb4b416df159af167ddde72240ed57f0c87
Henri Hulski <henri.hulski@gazeta.pl>
2016-10-28 00:13:47 +0200

    chore: move to Cerebral Monorepo
    Remove source code and link to Monorepo

--- src/chains/changeField.js
+++
@@ -1,8 +0,0 @@
-import validateField from '../factories/validateField'
-
-export default [
-  function updateValue ({input, state}) {
-    state.set(`${input.field}.value`, input.value)
-  },
-  validateField()
-]
